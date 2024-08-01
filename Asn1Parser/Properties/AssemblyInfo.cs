3e4e8808266718f277d080530a865cc3595d0787
Vadims Podāns <vpodans@sysadmins.lv>
2024-08-01 17:34:18 +0300

    enabled 'internal' visibility to unit test project

--- Asn1Parser/Properties/AssemblyInfo.cs
+++ Asn1Parser/Properties/AssemblyInfo.cs
@@ -1,4 +1,5 @@
 ﻿using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
@@ -20,6 +21,7 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("1b472f7a-4af5-45bf-aa38-4b7b35f63543")]
+[assembly: InternalsVisibleTo("Asn1Parser.Tests")]
 
 // Version information for an assembly consists of the following four values:
 //
