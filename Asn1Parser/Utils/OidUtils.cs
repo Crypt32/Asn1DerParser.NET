4bd04d3ac222d6768a1c40829f025cd1c8d2ec0b
Vadims Podāns <vpodans@sysadmins.lv>
2024-08-01 16:25:30 +0300

    extracted OID encoder into static method because it is reused by Asn1RelativeOid

---
+++ Asn1Parser/Utils/OidUtils.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+
+namespace SysadminsLV.Asn1Parser.Utils;
+static class OidUtils {
+    public static IEnumerable<Byte> EncodeOidArc(BigInteger arcValue) {
+        List<Byte> rawOid = [];
+        BigInteger temp = arcValue;
+        // calculate how many bits are occupied by the current integer value
+        Int16 bitLength = 0;
+        do {
+            temp = (BigInteger)Math.Floor((Double)temp / 2);
+            bitLength++;
+        } while (temp > 0);
+        // calculate how many additional bytes are required and encode each integer in a 7 bit.
+        // 8th bit of the integer is shifted to the left and 8th bit is set to 1 to indicate that
+        // additional bytes are related to the current OID arc. Details:
+        // http://msdn.microsoft.com/en-us/library/bb540809(v=vs.85).aspx
+        // loop may not execute if arc value is less than 128.
+        for (Int32 index = (bitLength - 1) / 7; index > 0; index--) {
+            rawOid.Add((Byte)(0x80 | ((arcValue >> (index * 7)) & 0x7f)));
+        }
+        rawOid.Add((Byte)(arcValue & 0x7f));
+
+        return rawOid;
+    }
+    public static String DecodeOidArc(IEnumerable<Byte> rawData) {
+        throw new NotImplementedException();
+    }
+}
