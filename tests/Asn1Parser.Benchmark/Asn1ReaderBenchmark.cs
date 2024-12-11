c1e7725510c1a1b8ff7d63181cca03fcbdecf4b3
Vadims Podāns <vpodans@sysadmins.lv>
2024-12-11 17:21:03 +0200

    added quick benchmark project to test implementations. Nothing fancy there.

---
+++ tests/Asn1Parser.Benchmark/Asn1ReaderBenchmark.cs
@@ -0,0 +1,62 @@
+﻿using Asn1Parser.Benchmark.Properties;
+using BenchmarkDotNet.Attributes;
+using SysadminsLV.Asn1Parser;
+
+namespace Asn1Parser.Benchmark;
+
+
+[MemoryDiagnoser]
+public class Asn1ReaderBenchmark {
+    readonly Asn1Reader _reader;
+
+    public Asn1ReaderBenchmark() {
+        Byte[] bytes = (Byte[])Resources.ResourceManager.GetObject("MiddleSizeCRL"); ;
+        _reader = new Asn1Reader(bytes);
+    }
+
+    [Benchmark(Baseline = true)]
+    public void Test1() {
+        _reader.BuildOffsetMap();
+        do {
+            _reader.GetPayload();
+        } while (_reader.MoveNext());
+    }
+    [Benchmark]
+    public void Test2() {
+        _reader.BuildOffsetMap();
+        do {
+            _reader.GetPayloadAsMemory();
+        } while (_reader.MoveNext());
+    }
+}
+[MemoryDiagnoser]
+public class Asn1FormatterBenchmark {
+    readonly Asn1Reader _reader;
+
+    public Asn1FormatterBenchmark() {
+        Byte[] bytes = (Byte[])Resources.ResourceManager.GetObject("MiddleSizeCRL");
+        _reader = new Asn1Reader(bytes);
+    }
+
+    [Benchmark(Baseline = true)]
+    public void TestArray() {
+        _reader.BuildOffsetMap();
+        do {
+            AsnFormatter.BinaryToString(_reader.GetTagRawData(), EncodingType.Base64);
+        } while (_reader.MoveNext());
+    }
+    [Benchmark]
+    public void TestSpan() {
+        _reader.BuildOffsetMap();
+        do {
+            AsnFormatter.BinaryToString(_reader.GetTagRawDataAsMemory().Span, EncodingType.Base64);
+        } while (_reader.MoveNext());
+    }
+    [Benchmark]
+    public void TestAsnReader() {
+        _reader.BuildOffsetMap();
+        do {
+            AsnFormatter.BinaryToString(_reader, EncodingType.Base64);
+        } while (_reader.MoveNext());
+    }
+}
---
+++ tests/Asn1Parser.Benchmark/CollectionPerfBenchmark.cs
@@ -0,0 +1,48 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace Asn1Parser.Benchmark;
+
+public class CollectionPerfBenchmark {
+    static readonly List<Byte> _list = [];
+    static readonly HashSet<Byte> _hashSet = [];
+
+    public CollectionPerfBenchmark() {
+        foreach (Byte b in Enumerable.Range(0, 31)) {
+            _list.Add(b);
+            _hashSet.Add(b);
+        }
+    }
+
+    [Benchmark(Baseline = true)]
+    public void Contains11() {
+        _list.Contains(0);
+    }
+    [Benchmark]
+    public void Contains21() {
+        _hashSet.Contains(0);
+    }
+    [Benchmark]
+    public void Contains12() {
+        _list.Contains(20);
+    }
+    [Benchmark]
+    public void Contains22() {
+        _hashSet.Contains(20);
+    }
+    [Benchmark]
+    public void Contains13() {
+        _list.Contains(30);
+    }
+    [Benchmark]
+    public void Contains23() {
+        _hashSet.Contains(30);
+    }
+    [Benchmark]
+    public void Contains14() {
+        _list.Contains(255);
+    }
+    [Benchmark]
+    public void Contains24() {
+        _hashSet.Contains(255);
+    }
+}
