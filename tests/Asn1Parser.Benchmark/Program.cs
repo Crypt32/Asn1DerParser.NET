1698df95a1e699ac30802bda3d56eb9e27549680
Vadims Podāns <vpodans@sysadmins.lv>
2024-12-11 17:21:03 +0200

    added quick benchmark project to test implementations. Nothing fancy there.

---
+++ tests/Asn1Parser.Benchmark/Program.cs
@@ -0,0 +1,13 @@
+﻿using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Reports;
+using BenchmarkDotNet.Running;
+
+namespace Asn1Parser.Benchmark;
+
+internal class Program {
+    static void Main(String[] args) {
+        IConfig config = DefaultConfig.Instance;
+        Summary summary = BenchmarkRunner.Run<Asn1ReaderBenchmark>(config);
+        summary = BenchmarkRunner.Run<Asn1FormatterBenchmark>(config);
+    }
+}
